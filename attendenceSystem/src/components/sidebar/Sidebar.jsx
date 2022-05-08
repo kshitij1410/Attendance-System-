f6e2584ad6cd3033f03089f905d7f74ee1e7abb7
Kshitij <kshitijkhandelwal14.2000@gmail.com>
2022-05-08 23:04:16 +0530

    first commit

---
+++ attendenceSystem/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,94 @@
+import "./sidebar.css";
+import {
+  LineStyle,
+  PermIdentity,
+  AttachMoney,
+  BarChart,
+  MailOutline,
+  DynamicFeed,
+  ChatBubbleOutline
+} from "@material-ui/icons";
+
+import { Link } from "react-router-dom";
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebarWrapper">
+
+
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Dashboard</h3>
+          <ul className="sidebarList">
+            <Link to="/home" className="link">
+              <li className="sidebarListItem active">
+                <LineStyle className="sidebarIcon" />
+                Home
+              </li>
+            </Link>
+
+          </ul>
+        </div>
+
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Quick Menu</h3>
+          <ul className="sidebarList">
+            <Link to="/attendance" className="link">
+              <li className="sidebarListItem">
+                <PermIdentity className="sidebarIcon" />
+                Attendance
+              </li>
+            </Link>
+
+            <Link to="/transactions" className="link">
+            <li className="sidebarListItem">
+              <AttachMoney className="sidebarIcon" />
+              Transactions
+            </li>
+            </Link>
+
+            <Link to="/reports" className="link">
+            <li className="sidebarListItem">
+              <BarChart className="sidebarIcon" />
+              Reports
+            </li>
+            </Link>
+          </ul>
+        </div>
+
+
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Notifications</h3>
+          <ul className="sidebarList">
+
+          <Link to="/mail" className="link">
+            <li className="sidebarListItem">
+              <MailOutline className="sidebarIcon" />
+              Mail
+            </li>
+            </Link>
+
+            <Link to="/feedback" className="link">
+            <li className="sidebarListItem">
+              <DynamicFeed className="sidebarIcon" />
+              Feedback
+            </li>
+
+            </Link>
+
+
+            <Link to="/messages" className="link">
+            <li className="sidebarListItem">
+              <ChatBubbleOutline className="sidebarIcon" />
+              Messages
+            </li>
+            </Link>
+          </ul>
+        </div>
+
+
+
+      </div>
+    </div>
+  );
+}
